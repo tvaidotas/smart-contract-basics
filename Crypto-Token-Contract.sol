b8d0c8ff8be4fbfd7e220fe8ac5b0b3b90edd1e9
tvaidotas <tadas.vaidotas@gmail.com>
2022-07-08 09:24:02 +0100

    beginings of a contract

---
+++ Crypto-Token-Contract.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+// all .sol files should start with a license declaration, can use MIT for oper source or UNLICENSED for private
+
+// defines our compiler and the version of it
+// ^ defines a range compile version after the non zero
+// digit upwards to the next increment of the non zero number
+pragma solidity ^0.8.4;
+
+// very first step, like defining a class and all the code goes inside
+// everything is explicitly declared
+// contracts can be inherited
+contract LearningSolidity {
+
+}
